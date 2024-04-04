3ec6cef6a7af75e8b2081bb69f1301cf883db2dd
Andrew Jason <107207708+MysticPl4y3r@users.noreply.github.com>
2024-04-04 16:22:13 +0530

    Update Hangman_cts.py

--- Hangman_cts.py
+++ Hangman_cts.py
@@ -1,5 +1,24 @@
 #Main 
 #Andrew
-def 
+def display_word_progress(self):
+        display = ""
+        for letter in self.secret_word:
+            if letter in self.guessed_letters:
+                display += letter
+            else:
+                display += "_"
+        print(display)
+
+def check_guess(self, guess):
+        if guess in self.guessed_letters:
+            print("You already guessed that letter.")
+            return
+
+        self.guessed_letters.add(guess)
+        if guess not in self.secret_word:
+            self.attempts_left -= 1
+            print("Incorrect guess!")
+        else:
+            print("Correct guess!")
 #Deepak -- take user guess
 #Abdullah handle win/loss Condition
