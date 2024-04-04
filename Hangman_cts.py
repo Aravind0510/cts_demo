6f3ab5017c9e7f994d9d7fc52871c5cef9ba14f5
jujftujytjugk <111437732+jujftujytjugk@users.noreply.github.com>
2024-04-04 16:14:47 +0530

    Update Hangman_cts.py

--- Hangman_cts.py
+++ Hangman_cts.py
@@ -2,4 +2,12 @@
 #Andrew
 def 
 #Deepak -- take user guess
+def take_user_guess(self):
+        while True:
+            guess = input("Guess a letter: ").lower()
+            if len(guess) == 1 and guess.isalpha():
+                return guess
+            else:
+                print("Invalid input. Please enter a single letter.")
+
 #Abdullah handle win/loss Condition
